1117f7cc87df0ff8cd2f3c28a7ab05933067d13f
Gabriel Conceição <gabriel.bc.dev@hotmail.com>
2023-08-28 09:24:14 -0300

    add component product-read

--- Crud/frontend/src/app/app.module.ts
+++ Crud/frontend/src/app/app.module.ts
@@ -24,6 +24,11 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { ProductReadComponent } from './components/product/product-read/product-read.component';
+import { ProductRead2Component } from './components/product/product-read2/product-read2.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
 
 @NgModule({
   declarations: [
@@ -33,7 +38,9 @@ import { MatInputModule } from '@angular/material/input';
     NavComponent,
     HomeComponent,
     ProductCrudComponent,
-    ProductCreateComponent
+    ProductCreateComponent,
+    ProductReadComponent,
+    ProductRead2Component
   ],
   imports: [
     BrowserModule,
@@ -48,7 +55,10 @@ import { MatInputModule } from '@angular/material/input';
     HttpClientModule,
     FormsModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Crud/frontend/src/app/components/product/product-create/product-create.component.ts
+++ Crud/frontend/src/app/components/product/product-create/product-create.component.ts
@@ -11,6 +11,7 @@ import { Product } from '../product.model';
 export class ProductCreateComponent implements OnInit {
 
   product: Product = {
+    id: 0,
     name: '',
     price: 0
   }
